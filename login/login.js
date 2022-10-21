a4e4eb8a3c412ae312cb4051a2dbc0e8890498be
AndresGzz <andres_lgg@hotmail.com>
2022-10-21 05:02:26 -0500

    test 8

--- login/login.js
+++ login/login.js
@@ -10,7 +10,7 @@
     let correctPass="password"
     
     if(user == correctUser && pass==correctPass){
-        document.location="{{ /TiendaTEMA3}}{{/store.html }}"
+        document.location="{{ /store.html }}"
     }else{
         incorrect.removeAttribute('hidden')
         userField.focus()
